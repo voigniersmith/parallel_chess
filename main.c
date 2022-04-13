c694b74b0a0b4e78b4e47f14fbcd19a5347b2edf
smit3407 <smit3407@purdue.edu>
2022-04-13 14:41:21 -0400

    Added Basic Inp Parse

--- main.c
+++ main.c
@@ -3,29 +3,57 @@
 #include <string.h>
 #include <ctype.h>
 
+struct board {
+	char* layout;
+	char turn;
+	char* castling;
+	char* enpassant;
+	int halfmove;
+	int fullmove;
+};
+
+int parse_inp (char*);
 void print_board (char*);
 
 // Driver code
 int main (int argc, char* argv) {
 	printf("Welcome to chess!\n");
-	
-	char* layout = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR\0";
-	print_board(layout);
 
-	char inp[100];
-	while(strcmp(inp, "exit") != 0) {
-		printf("Enter a layout: ");
-		scanf("%s", inp);
-		// Need to check input here
-		print_board(inp);
+	int exit_status = 0;
+	struct board* b = (struct board*) malloc(sizeof(struct board));
+	b->layout = (char*) malloc(sizeof(char) * 100);
+	strcpy(b->layout, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR\0");
+	
+	while (exit_status == 0) {
+		print_board(b->layout);
+		printf("chess > ");
+		scanf("%s", b->layout);
+		exit_status = parse_inp(b->layout);
 	}
+
+	printf("Thanks for playing!\n");
+
+	return 0;
+}
+
+// Function to parse the input
+int parse_inp(char* s) {
+	if (strcmp(s, "exit") == 0) return 1;
+
+	if (s[0] < 'a' || s[0] > 'h' ||
+		s[1] < '0' || s[1] > '9' ||
+		s[2] < 'a' || s[2] > 'h' ||
+		s[3] < '0' || s[3] > '9')
+		return 1;
+
+
 
 	return 0;
 }
 
 // Function to print the board based on the layout
 void print_board (char* layout) {
-	int rows = 8, columns = 0, spaces = 0;
+	int rows = 7, columns = 0, spaces = 0;
 	int size = strlen(layout);
 	
 	printf("\t  +------------------------+\n");
@@ -36,9 +64,9 @@ void print_board (char* layout) {
 			if (isdigit(layout[i])) {
 				spaces = atoi(&layout[i]);
 				while (spaces > 0) {
-					if (columns % 2 == 0 && rows % 2 == 0) {
+					if (columns % 2 == 0 && rows % 2 == 1) {
 						printf(":::");
-					} else if (columns % 2 == 1 && rows % 2 == 1) {
+					} else if (columns % 2 == 1 && rows % 2 == 0) {
 						printf(":::");
 					}else {
 						printf("   ");
@@ -48,10 +76,7 @@ void print_board (char* layout) {
 				}
 				i++;
 			} else {
-				if (isupper(layout[i]))
-					printf("<%c>", layout[i++]);
-				else
-					printf("[%c]", layout[i++]);
+				printf(" %c ", layout[i++]);
 				columns++;
 			}
 		}
@@ -63,12 +88,12 @@ void print_board (char* layout) {
 	printf("\t    a  b  c  d  e  f  g  h  \n");
 	
 	// The Standard Layout	
-	// printf("\t8 |[r][n][b][q][k][b][n][r]|\n");
-	// printf("\t7 |[p][p][p][p][p][p][p][p]|\n");
+	// printf("\t8 | r  n  b  q  k  b  n  r |\n");
+	// printf("\t7 | p  p  p  p  p  p  p  p |\n");
 	// printf("\t6 |   :::   :::   :::   :::|\n");
 	// printf("\t5 |:::   :::   :::   :::   |\n");
 	// printf("\t4 |   :::   :::   :::   :::|\n");
 	// printf("\t3 |:::   :::   :::   :::   |\n");
-	// printf("\t2 |<P><P><P><P><P><P><P><P>|\n");
-	// printf("\t1 |<R><N><B><Q><K><B><N><R>|\n");
+	// printf("\t2 | P  P  P  P  P  P  P  P |\n");
+	// printf("\t1 | R  N  B  Q  K  B  N  R |\n");
 }
